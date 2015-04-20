12fa64c6be19c5199b12696e85336cef520682e7
Akihiko Tanikawa <pureodio1109@gmail.com>
2015-04-20 22:23:05 +0900

    Fix make.bat

--- make.bat
+++ make.bat
@@ -1,14 +1,15 @@
 @echo off
+set dist=tree
 pushd %~dp0
-call :Build windows amd64 tree.exe
-call :Build windows 386 tree.exe
+call :Build windows amd64 %dist%.exe
+call :Build windows 386 %dist%.exe
 pause
 exit
 
 :Build
 set GOOS=%~1
 set GOARCH=%~2
-set out_dir=build\%GOOS%\%GOARCH%
+set out_dir=%dist%\%GOOS%\%GOARCH%
 if not exist %out_dir%\ ( mkdir %out_dir% )
 echo GOOS=%GOOS% GOARCH=%GOARCH% build
 go build -o %out_dir%\%~3
